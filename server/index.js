8d8a13ede48e03869eee9297f35e2a7cf4970f7d
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-15 14:42:00 -0600

    updated affirmations button

--- server/index.js
+++ server/index.js
@@ -31,6 +31,39 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+app.get("/api/affirmations", (req, res) => {
+  const affirmations = ["I am inspired by things happening all around me.",
+					 "I am grateful for the people I have in my life.",
+					 "I grow and improve every day.",
+           "I treat myself kindly and with compassion.",
+           "I am courageous.",
+           "I am capable of standing up for myself.",
+           "I approve of myself.",
+           "I love myself fully.",
+           "I have value.",
+           "I am at peace with who I am.",
+           "I am allowed to take care of myself.",
+           "I am worthy of love.",
+           "I am enough.",
+           "My needs and wants are important.",
+           "​I’m worthy of respect and kindness.",
+           "I choose to celebrate my good qualities",
+           "I'm doing my best and that is enough",
+           "I am good enough.",
+           "I am in control of how I live my life. ",
+           "I am capable of making my own decisions",
+           "My future is bright",
+           "I deserve success",
+           "I give myself permission to do what is right for me"
+  ];
+
+  // choose random affirmation
+  let randomIndex = Math.floor(Math.random() * affirmations.length);
+  let randomAffirmations = affirmations[randomIndex];
+
+  res.status(200).send(randomAffirmations);
+  
+});
 
 app.get("/api/fortune", (req, res) => {
   const fortunes = ["A beautiful, smart, and loving person will be coming into your life.",
